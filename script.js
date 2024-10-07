b305053135be141a154ca8c95990e9eb4debb0ad
abeer-rozba <abeerrozba3@gmail.com>
2024-10-07 20:45:18 +0300

    Compare function works for two cards

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
 // constants
-const cards = []
+const cards = [] // will save selected cards for compare function
 
 
 // variables
-
+let match = false
 
 // cached elements
-const cardElements = document.querySelectorAll(".card")
+const cardElements = document.querySelectorAll(".card") // saves all cards in memory
 
 // functions
-const init = (event) => {
+const init = (event) => { // main function
   flipCard(event)
 }
   
 
 const flipCard = (event) => {
-  const firstCard = event.target.parentElement
+  const firstCard = event.target.parentElement // saves the parent div of selected card
+  cards.push(firstCard.classList[1])
   firstCard.classList.toggle("flipCard")
+  compare()
 }
 
+const compare = () => {
+  if (cards[0] === cards[1]) {
+    match = true
+  }
+  else { match = false }
+}
 
 // event listeners
 cardElements.forEach((card) => {
-  cards.push(card.id)
   card.addEventListener("click", init)
 })
