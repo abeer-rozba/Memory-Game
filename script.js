d5f6a40e88ff66b8c844b5aecca3f436f021ac32
abeer-rozba <abeerrozba3@gmail.com>
2024-10-08 19:06:09 +0300

    One card stops listening for clicks when matched

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // constants
-const cards = [] // will save selected cards for compare function
+const cards = [] // will save selected cards class for compare function
 
 
 // variables
 let match = false
+let parentDiv = ""
 
 // cached elements
 const cardElements = document.querySelectorAll(".card") // saves all cards in memory
@@ -11,24 +12,27 @@ const cardElements = document.querySelectorAll(".card") // saves all cards in me
 // functions
 const init = (event) => { // main function
   flipCard(event)
+  compare()
 }
   
 
 const flipCard = (event) => {
-  const firstCard = event.target.parentElement // saves the parent div of selected card
-  cards.push(firstCard.classList[1])
-  firstCard.classList.toggle("flipCard")
-  compare()
+  parentDiv = event.target.parentElement // saves the parent div of selected card
+  cards.push(parentDiv.classList[1]) // saves second class to compare
+  parentDiv.classList.toggle("flipCard") // flips the card
 }
 
 const compare = () => {
-  if (cards[0] === cards[1]) {
-    match = true
+  if (cards[0] === cards[1]) { // compares card classes to determine a match
+    match = true // updates the match value
+    parentDiv.removeEventListener("click", init) // disables clicking on the current card again
   }
   else { match = false }
+  console.log(match);
+  console.dir(parentDiv);
 }
 
 // event listeners
-cardElements.forEach((card) => {
+cardElements.forEach((card) => { // listens for user clicks on cards
   card.addEventListener("click", init)
 })
