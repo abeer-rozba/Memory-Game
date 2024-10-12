ba7aedea295c81207f9a9a46cd9acf5b7db29f06
abeer-rozba <abeerrozba3@gmail.com>
2024-10-12 20:25:28 +0300

    successfully flipped non-matching cards back

--- script.js
+++ script.js
@@ -52,27 +52,25 @@ cardElements.forEach((card) => { // listens for user clicks on cards
 
 
 // constants
-const cards = ["A", "A", "B", "B", "C", "C", "D", "D"]
+const cards = ["A", "A", "B", "B", "C", "C", "D", "D"] // stores the cards
 
 
 // variables
-let firstCard = null, secondCard
+let firstCard = null, secondCard, firstClickedCard, cardClass
 
 
 // cached elements
-const containerElement = document.querySelector(".container")
+const containerElement = document.querySelector(".container") // to append cards to
 
 
 // functions
-const play = () => {
-  setUpCards()
-}
-
 const setUpCards = () => {
-  cards.forEach((card) => {
+  let counter = 0 // to assign a unique id to each card
+  cards.forEach((card) => { // creates as much cards as there are in the cards array
     const mainCard = document.createElement("div")
     containerElement.appendChild(mainCard)
-    mainCard.classList.add("card") 
+    mainCard.classList.add("card")
+    mainCard.id = counter++ // id is only on the card that will listen for clicks
     const cardInner = document.createElement("div")
     mainCard.appendChild(cardInner)
     cardInner.classList.add("inner")
@@ -87,31 +85,63 @@ const setUpCards = () => {
 }
 
 const flipCard = (event) => {
-  event.currentTarget.children[0].classList.toggle("flip")
-  if (!firstCard) { 
-    firstCard = event.currentTarget.textContent
+  cardClass = event.currentTarget.children[0] // to access the card classes easily
+  cardClass.classList.toggle("flip") // flips the card div
+  if (firstCard === null) { // if no card was clicked previously
+    firstClickedCard = event.currentTarget.id // fetch the card id for the second condition
+    console.log(firstClickedCard);
+    firstCard = event.currentTarget // store the clicked card
+    console.log(firstCard);
+  }
+  else if (event.currentTarget.id === firstClickedCard) { // check if the same card was clicked twice in a row
+    firstCard = null // reset the card value and id
+    firstClickedCard = null
+    console.log(firstClickedCard);
+    console.log(firstCard);
   }
   else {
-    secondCard = event.currentTarget.textContent
+    secondCard = event.currentTarget // store the second card to compare
     compareCards(firstCard, secondCard)
   }
 }
 
 const compareCards = (firstCard, secondCard) => {
-  if (firstCard === secondCard)
+  if (firstCard.textContent === secondCard.textContent) // compare the cards values after ensuring they're not the same card
   {
-    console.log("match");
+    console.log("match"); // removeEventListener is not working to disable clicking on matched cards!!
   }
-  else if (firstCard !== secondCard) {
-    console.log("no match");
+  else if (firstCard.textContent !== secondCard.textContent) { // flip non-matching cards back after a delay
+    setTimeout(() => {
+      firstCard.children[0].classList.remove("flip") // works
+      secondCard.children[0].classList.remove("flip")
+      firstCard = null // those three lines are clearing the variables but the first card to be clicked afterwards
+      secondCard = null // is still considered the second card and skips immediately to the comparing function
+      firstClickedCard = null
+      console.log(firstCard);
+      console.log(secondCard);
+      console.log(firstClickedCard);
+    }, 1000)
   }
 }
 
-play()
+setUpCards()
 // event listeners
-const cardElements = document.querySelectorAll(".card")
-const cardElementsArray = Array.from(cardElements)
+const cardElements = document.querySelectorAll(".card") // cache cards that were created dynamically
+// this only works if it was placed here for some reason (?) doesn't work if I move this line somewhere else
+const cardElementsArray = Array.from(cardElements) // convert node list to an array
 
-cardElementsArray.forEach((card) => {
+cardElementsArray.forEach((card) => { // listen for clicks on every card
   card.addEventListener("click", (event) => flipCard(event))
 })
+
+/*
+
+What am I struggling with?
+
+- Matching cards still listen for clicks. How do I disable it?
+- If cards don't match, the cards values do not reset. Instead, the first card to be clicked after the timeout
+will still be compared with the first card from before the timeout. This ruins the whole comparison process. Why?
+- I read about the shuffle method online but I can't understand how it works.
+
+
+*/
